a0846e4379fd98e4bdab2dc9ccf1e28d863167c0
Bkhangg <tkame1084@gmail.com>
2026-02-23 09:40:01 +0700

    Hoàn thành login, register bằng breeze có thông báo toastr

---
+++ lang/vi/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Các thông báo xác thực (Authentication)
+    |--------------------------------------------------------------------------
+    |
+    | Các dòng thông báo này được dùng khi đăng nhập / xác thực người dùng.
+    | Bạn có thể chỉnh lại cho phù hợp với hệ thống của mình.
+    |
+    */
+
+    'failed' => 'Thông tin đăng nhập không chính xác.',
+    'password' => 'Mật khẩu bạn nhập không đúng.',
+    'throttle' => 'Bạn đã đăng nhập sai quá nhiều lần. Vui lòng thử lại sau :seconds giây.',
+
+];
---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import "./bootstrap";
+import toastr from "toastr";
+import "toastr/build/toastr.min.css";
+
+import Alpine from "alpinejs";
+
+window.Alpine = Alpine;
+
+Alpine.start();
